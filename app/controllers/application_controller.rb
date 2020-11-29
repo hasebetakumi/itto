23c4dfe8045ea01728da8d42a2c1290d978119a2
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-11-29 11:50:48 +0000

    nameカラム　ストロングパラメーター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :permitted_parameter, if: :devise_controller?
+    
+    def permitted_parameter
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
