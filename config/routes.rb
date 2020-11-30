094f510cbd7567cf0005a83540d395f2941ffa8c
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-11-30 01:34:42 +0000

    root微調整

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     root 'mains#index'
     
     resources :reports, only: :index
-    resources :shools, only: [:index]
+    resources :schools, only: [:index]
 end
