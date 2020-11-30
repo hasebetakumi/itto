4434ff371052006210df47039e5863ff8551445c
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-11-30 00:33:13 +0000

    schoolコントローラ生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_113049) do
+ActiveRecord::Schema.define(version: 2020_11_30_002057) do
+
+  create_table "schools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "classifying"
+    t.string "shool"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
