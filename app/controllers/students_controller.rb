c14da1da49c872ef6a8e73ece9f53e8fe7e2811a
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-11-30 05:16:15 +0000

    student create method 設定

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,14 @@ class StudentsController < ApplicationController
         @students = Student.all
         @student = Student.new
     end
+    
+    def create
+        School.create(student_params)
+        @schools = School.all
+    end
+    
+    private
+    def student_params
+        params.require(:student).permit(:classifying, :grade, :school_id, :student, :student_type)
+    end
 end
