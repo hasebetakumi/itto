40985c0470f743feb917b172e8075f4ab2a5ddcd
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-11-30 04:37:29 +0000

    studentコントローラー生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_002057) do
+ActiveRecord::Schema.define(version: 2020_11_30_043308) do
 
   create_table "schools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "classifying"
     t.string "school"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "classifying"
+    t.integer "grade"
+    t.integer "school_id"
+    t.string "student"
+    t.integer "student_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
