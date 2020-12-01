8701c91ba562fbdc4f8722e6e12921805f74b4eb
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-12-01 00:51:21 +0000

    reportテーブル変更

--- db/migrate/20201201002054_create_reports.rb
+++ db/migrate/20201201002054_create_reports.rb
@@ -4,8 +4,14 @@ class CreateReports < ActiveRecord::Migration[5.2]
       t.integer :classifying
       t.integer :student_id
       t.integer :user_id
-      t.string :subject
+      t.integer :subject_id
+      t.integer :class_textbook
+      t.integer :class_start_page
+      t.integer :class_end_page
       t.text :report
+      t.integer :home_textbook
+      t.integer :home_start_page
+      t.integer :home_end_page
       t.integer :understanding
       t.timestamps
     end
