51d4b9604d0e3b2c7002a9ffc14dc68fdec13bb7
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-12-05 23:49:04 +0000

    種別順に並べ替え

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,12 +1,12 @@
 class SchoolsController < ApplicationController
     def index
-        @schools = School.all
+        @schools = School.all.order(classifying: :ASC)
         @school = School.new
     end
     
     def create
         School.create(school_params)
-        @schools = School.all
+        @schools = School.all.order(classifying: :ASC)
     end
     
     private
