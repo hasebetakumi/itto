fc74d9ab5b6b644bd95448a9cdb158ac5af74e44
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-12-05 00:10:13 +0000

    student_type_id permit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,8 @@
 class StudentsController < ApplicationController
     def index
-        @elementary_students = Student.where(classifying: 1)
-        @junior_high_school_students = Student.where(classifying: 2)
-        @high_school_students = Student.where(classifying: 3)
+        @elementary_students = Student.where(classifying: 1).includes(:school)
+        @junior_high_school_students = Student.where(classifying: 2).includes(:school)
+        @high_school_students = Student.where(classifying: 3).includes(:school)
         @student = Student.new
     end
     
@@ -15,6 +15,6 @@ class StudentsController < ApplicationController
     
     private
     def student_params
-        params.require(:student).permit(:classifying, :grade, :school_id, :student, :student_type)
+        params.require(:student).permit(:classifying, :grade, :school_id, :student, :student_type_id)
     end
 end
