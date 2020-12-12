bfe29cc3d083c0b87a86d074a47692c50c41598d
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-12-12 12:06:44 +0000

    検索機能設定

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,10 @@
 class ResultsController < ApplicationController
+    def index
+        @results = Result.where(student_id: params[:student_keyword], grade: params[:grade_keyword], semester_id: params[:semester_keyword])
+        @student = params[:student_keyword]
+        @grade = params[:grade]
+        @semester = params[:semester_keyword]
+        @students = Student.all
+        @semesters = Semester.all
+    end
 end
