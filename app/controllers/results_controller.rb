e7b872f826021ed10ecf13bff83d82c93a1f771f
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-12-13 00:42:17 +0000

    成績新規登録用コントローラー設定

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -9,4 +9,18 @@ class ResultsController < ApplicationController
         @students = Student.all
         @semesters = Semester.all
     end
+    
+    def new
+        @result = Result.new
+    end
+    
+    def create
+        Result.create(create_params)
+        redirect_to results_path
+    end
+    
+    private
+    def create_params
+        params.require(:result).permit(:student_id, :grade, :semester_id, :english, :math, :japanese, :science, :social, :art, :pe, :techhome, :music).merge(user_id: current_user.id)
+    end
 end
