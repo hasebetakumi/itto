68a616971f44de1b87cf1ef531affa01b346d829
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-12-15 04:57:11 +0000

    indexメソッド設定

--- app/controllers/testresults_controller.rb
+++ app/controllers/testresults_controller.rb
@@ -1,6 +1,18 @@
 class TestresultsController < ApplicationController
     def index
+        if params[:student_keyword].present? and params[:grade_keyword].present? and params[:test_keyword].present?
+            @results = Result.where(student_id: params[:student_keyword], grade: params[:grade_keyword], semester_id: params[:test_keyword]).includes(:student, :user, :test)
+            @searchparameters = [params[:student_keyword], params[:grade_keyword], params[:test_keyword]]
+            student = Student.find(params[:student_keyword])
+            @student = student.name
+            test = Test.find(params[:test_keyword])
+            @semester = test.test
+        else
+            @aaa = 1
+        end
         
+        @students = Student.all
+        @tests = .all
     end
     
     def new
