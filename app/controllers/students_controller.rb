01127dbdd6e4abcbd0aa715ed0b6ba0968b5fba7
EC2 Default User <ec2-user@ip-172-31-17-245.ap-northeast-1.compute.internal>
2020-12-17 00:41:39 +0000

    showメソッド設定

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,23 @@ class StudentsController < ApplicationController
         @high_school_students = Student.where(classifying: 3).includes(:school, :student_type).order(grade: :ASC, family_name_kana: :ASC)
     end
     
+    def show
+        @student = Student.find(params[:id])
+        @reports = Report.where(student_id: params[:id]).order(created_at: :DESC)
+        
+        user_ids = Report.where(student_id: params[:id]).group(:user_id).order('count_user_id DESC').count(:user_id)
+        @users = []
+        @user_counts = []
+        
+            user_ids.each do |user_id|
+                @users << User.find(user_id[0])
+                @user_counts << user_id[1]
+            end
+        
+        @results = Result.where(student_id: params[:id]).order(created_at: :DESC)
+        @testresults = Testresult.where(student_id: params[:id]).order(created_at: :DESC)
+    end
+    
     private
     def student_params
         params.require(:student).permit(:classifying, :grade, :school_id, :year, :family_name, :given_name, :family_name_kana, :given_name_kana, :student_type_id)
