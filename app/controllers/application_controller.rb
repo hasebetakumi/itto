163927edfef9ca37b0528a00f56b6dfe93731d97
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-05 05:18:24 +0000

    権限のないユーザーはログイン画面にリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
-    
-    
+    before_action :sign_out_user,  if: :user_signed_in?
     before_action :permitted_parameter, if: :devise_controller?
     
     def permitted_parameter
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+    
+    
+    protected
+    def sign_out_user
+      sign_out_and_redirect(current_user) unless current_user.right?
     end
 end
