5827ec3497c7746111481df12e81b11ec5a8734b
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-06 14:38:26 +0000

    target create メソッド設定

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -2,4 +2,14 @@ class TargetsController < ApplicationController
     def new
         @target = Target.new
     end
+    
+    def create
+        Target.create(create_params)
+        redirect_to student_path(params.require(:target)[:student_id])
+    end
+    
+    private
+    def create_params
+        params.require(:target).permit(:student_id, :exam_coverage_first, :exam_coverage_last, :score, :full_mark).merge(user_id: current_user.id)
+    end
 end
