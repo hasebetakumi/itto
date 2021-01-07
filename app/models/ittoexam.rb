5140d0c4db68fe1ceef3325a1f8ffdb8b6357211
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-07 04:47:34 +0000

    ittoexamモデル生成

---
+++ app/models/ittoexam.rb
@@ -0,0 +1,2 @@
+class Ittoexam < ApplicationRecord
+end
---
+++ db/migrate/20210107044714_create_ittoexams.rb
@@ -0,0 +1,8 @@
+class CreateIttoexams < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ittoexams do |t|
+
+      t.timestamps
+    end
+  end
+end
