03da3316abc155492c413f6714f87af5ba82a299
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-07 07:15:29 +0000

    ittoexams indexメソッド設定

--- app/controllers/ittoexams_controller.rb
+++ app/controllers/ittoexams_controller.rb
@@ -1,2 +1,17 @@
 class IttoexamsController < ApplicationController
+    def index
+        if params[:year_keyword].present? and params[:month_keyword].present?
+            ittoexams = Ittoexam.where(year: params[:year_keyword])
+            @ittoexams = ittoexams.where(month: params[:month_keyword]).includes(:student, :user).order("students.family_name_kana desc", "students.grade asc", "students.classifying asc")
+        else
+            @ittoexams = []
+        end
+        
+        require 'date'
+        day = Date.today
+        year = day.year
+        @thisyear = year
+        @nextyear = year + 1 
+        @thismonth = day.month
+    end
 end
