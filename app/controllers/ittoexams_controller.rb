9bef03b349435f4bde3706d36c5506c375cdf95c
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-07 10:41:17 +0000

    ittoexams edit update メソッド設定

--- app/controllers/ittoexams_controller.rb
+++ app/controllers/ittoexams_controller.rb
@@ -37,8 +37,30 @@ class IttoexamsController < ApplicationController
         end
     end
     
+    def edit
+        @ittoexam = Ittoexam.find(params[:id])
+        
+        require 'date'
+        day = Date.today
+        year = day.year
+        @thisyear = year
+        @nextyear = year + 1 
+        @lastyear = year - 1
+        @thismonth = day.month
+    end
+    
+    def update
+        ittoexam = Ittoexam.find(params[:id])
+        ittoexam.update(update_params)
+        redirect_to ittoexams_path
+    end
+    
     private
     def create_params
+        params.require(:ittoexam).permit(:year, :month, :student_id, :english_score, :english_full_mark, :japanese_score, :japanese_full_mark, :science_score, :science_full_mark, :social_score, :social_full_mark).merge(user_id: current_user.id)
+    end
+    
+    def update_params
         params.require(:ittoexam).permit(:year, :month, :student_id, :english_score, :english_full_mark, :japanese_score, :japanese_full_mark, :science_score, :science_full_mark, :social_score, :social_full_mark).merge(user_id: current_user.id)
     end
 end
