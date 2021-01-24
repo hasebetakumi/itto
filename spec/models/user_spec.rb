b8ec6c7abd3d11dcdb81513cce1b28362857ff16
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-24 07:57:16 +0000

    usermodel 最初のテスト設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
-it "姓、名、メール、パスワードがある場合、有効である"
-
+    it "名前とメールアドレスとパスワードがあれば登録できる" do
+        user = User.new(
+        name: "テスト",
+        email: "testman@example.com",
+        password: "password",
+        )
+        expect(user).to be_valid
+    end
 end
