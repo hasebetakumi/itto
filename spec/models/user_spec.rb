1cfb110e497c3f2b9882357da5a359669605435a
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-24 10:54:26 +0000

    user modelのテストにfactory設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     it "名前とメールアドレスとパスワードがあれば登録できる" do
-        user = User.new(
-        name: "テスト",
-        email: "testman@example.com",
-        password: "password",
-        )
-        expect(user).to be_valid
+        expect(FactoryBot.create(:user)).to be_valid
     end
 end
