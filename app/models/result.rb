96e6145d553c3ae74b7100be42d72c4ff5c311e2
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-25 05:23:17 +0000

    resultバリデーション設定　テストも記述

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,4 +2,10 @@ class Result < ApplicationRecord
   belongs_to :user
   belongs_to :student
   belongs_to :semester
+  
+  validates :student_id, presence: true
+  validates :grade, presence: true
+  validates :semester_id, presence: true
+
+
 end
