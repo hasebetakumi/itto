0283d1a74c797b3033f6554705174bf530e0c2f4
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-25 06:13:22 +0000

    studentバリデーション設定　テストも記述

--- test/factories/student.rb
+++ test/factories/student.rb
@@ -4,6 +4,9 @@ FactoryBot.define do
         association :student_type
         classifying {"1"}
         grade {"6"}
-        family_name {"テスト"}
+        family_name {"試験"}
+        given_name {"太郎"}
+        family_name_kana {"しけん"}
+        given_name_kana {"たろう"}
     end
 end
