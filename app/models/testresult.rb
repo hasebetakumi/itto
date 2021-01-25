892cf344ee784963db82fbafd8f32dfb666f5080
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-25 05:32:17 +0000

    testresultバリデーション設定　テストも記述

--- app/models/testresult.rb
+++ app/models/testresult.rb
@@ -2,4 +2,8 @@ class Testresult < ApplicationRecord
   belongs_to :user
   belongs_to :student
   belongs_to :test
+  
+  validates :student_id, presence: true
+  validates :grade, presence: true
+  validates :test_id, presence: true
 end
--- spec/models/testresult_spec.rb
+++ spec/models/testresult_spec.rb
@@ -2,8 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Testresult, type: :model do
     describe '定期試験登録' do
-        it "student user test grade を登録できる" do
-            expect(FactoryBot.create(:testresult)).to be_valid
+        context "バリデーション" do
+            it "student user test grade を登録できる" do
+                expect(FactoryBot.create(:testresult)).to be_valid
+            end
+            
+            it "student_idがないと登録できない" do
+                expect(FactoryBot.build(:testresult, student_id: "")).to_not be_valid
+            end
+            
+            it "gradeがないと登録できない" do
+                expect(FactoryBot.build(:testresult, grade: "")).to_not be_valid
+            end
+            
+            it "test_idがないと登録できない" do
+                expect(FactoryBot.build(:testresult, test_id: "")).to_not be_valid
+            end
         end
     end
 end
