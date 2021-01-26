3b92d4b9413bc9a284a3e533f9813f44c57a9d97
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-26 01:27:47 +0000

    report コントローラーテスト記述

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     name {"test"}
     sequence(:email) { |n| "test#{n}@example.com"}
     password {"password"}
+    right {"1"}
+    confirmed_at {"2021-01-01 00:00:00"}
   end
 end
