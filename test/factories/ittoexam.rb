fa5ec20ba3aca92b9bb663bbb0d9a19a81d70855
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-01-27 00:13:19 +0000

    factory ittoexam微調整

--- test/factories/ittoexam.rb
+++ test/factories/ittoexam.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
     factory :ittoexam do
-        year {"2020"}
-        month {"1"}
         association :user
         association :student
+        year {"2020"}
+        month {"1"}
+        
     end
 end
