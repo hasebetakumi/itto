ffc1389aa2112f65f768e8dda9aa72dc315c5dcb
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-02-09 09:51:28 +0000

    user modelへのアソシエーション、テスト記述

--- app/models/highfile.rb
+++ app/models/highfile.rb
@@ -1,5 +1,5 @@
 class Highfile < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
   belongs_to :student
   
   validates :student_id, presence: true
---
+++ spec/features/highfile_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'Highfile', type: :feature do
+  before do
+    @user = FactoryBot.create(:user)
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: @user.email.to_s
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+      
+    @highfile = FactoryBot.create(:highfile, user_id: @user.id.to_s)
+    @student = FactoryBot.create(:student, classifying: '3', grade: '3')
+      
+    visit root_path
+    click_on '成績・面談(高)'
+  end
+    
+  describe '登録' do
+    it '新規登録できる' do
+      select '高　3年　試験 太郎', from: '生徒'
+      fill_in '登録名', with: '高校生面談'
+      attach_file "highfile[image]", "app/assets/images/git.svg"
+      click_on '登録'
+      expect(page).to have_content '高校生面談'
+    end
+  end
+end
