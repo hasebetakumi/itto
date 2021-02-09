c29c46d39fafd99349a597143a62e620a30ea370
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-02-09 04:03:40 +0000

    highfile newファイル設定

--- app/controllers/highfiles_controller.rb
+++ app/controllers/highfiles_controller.rb
@@ -1,2 +1,28 @@
 class HighfilesController < ApplicationController
+    def new
+        # 新規登録用
+        @highfile = Highfile.new
+        
+        # 一覧用データ渡し
+        @highfiles = Highfile.all.order(created_at: :DESC)
+    end
+    
+    def create
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+    
+    private
+    
 end
