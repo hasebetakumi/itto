87a26b7b444d9e94638c08a739342d78966d251b
EC2 Default User <ec2-user@ip-172-31-19-100.ap-northeast-1.compute.internal>
2021-02-09 03:44:31 +0000

    highfile マイグレーション記述

--- db/migrate/20210209033415_create_highfiles.rb
+++ db/migrate/20210209033415_create_highfiles.rb
@@ -1,7 +1,10 @@
 class CreateHighfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :highfiles do |t|
-
+      t.integer :student_id
+      t.string :image_name
+      t.text :image
+      t.integer :user_id
       t.timestamps
     end
   end
