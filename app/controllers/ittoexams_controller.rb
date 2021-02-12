16a57dfd226250727da34330f53bdf56da3f7a46
hasebetakumi <nba.syuu.k@gmail.com>
2021-02-12 06:47:56 +0000

    ittoexam修正

--- app/controllers/ittoexams_controller.rb
+++ app/controllers/ittoexams_controller.rb
@@ -16,41 +16,46 @@ class IttoexamsController < ApplicationController
       @searchparameters = [@thisyear, @thismonth]
     end
 
-      # 登録されていない生徒の割り出し
+      # 各教科登録されていない生徒の割り出し
     @en_none_ittoexams = @ittoexams.where(english_score: nil).includes(:student)
     @ja_none_ittoexams = @ittoexams.where(japanese_score: nil).includes(:student)
     @sc_none_ittoexams = @ittoexams.where(science_score: nil).includes(:student)
     @so_none_ittoexams = @ittoexams.where(social_score: nil).includes(:student)
-
+    
+    # 提出されている人のid取り出し
     ittoexam_ids = []
     @ittoexams.each do |ittoexam|
       ittoexam_ids << ittoexam.student_id
     end
+    # 小中学生全員のid取り出し
     student_ids = []
     students = Student.where(classifying: [1, 2])
     students.each do |student|
       student_ids << student.id
     end
+    # 小中学生全員ー提出されている人＝未提出者のid
     ittoexam_ids.each do |ittoexam_id|
       @none_student_ids = student_ids.delete_if { |id| id == ittoexam_id }
     end
     @none_students = []
+    # 未提出者のidがあれば、idから生徒を割り出し、順番を変えて取得
     if @none_student_ids.present?
       @none_student_ids.each do |none_student_id|
         @none_students << Student.find(none_student_id)
         @none_students = @none_students.sort { |a, b| [a[:classifying], a[:grade], a[:family_name_kana], a[:given_name_kana]] <=> [b[:classifying], b[:grade], b[:family_name_kana], b[:given_name_kana]] }
       end
-    else
-      unless params[:year_keyword].blank? and params[:month_keyword].blank?
-        @student_ids = []
-        students = Student.where(classifying: [1, 2])
-        students.each do |student|
+    end
+    # そもそも月に誰も登録されていないなら、@none_studentsに全員入れて渡す。
+    if @ittoexams.blank?
+      @student_ids = []
+      students = Student.where(classifying: [1, 2])
+      students.each do |student|
           @student_ids << student.id
-        end
-        @student_ids.each do |student_id|
+      end
+      @student_ids.each do |student_id|
           @none_students << Student.find(student_id)
-          @none_students = @none_students.sort { |a, b| [a[:classifying], a[:grade], a[:family_name_kana], a[:given_name_kana]] <=> [b[:classifying], b[:grade], b[:family_name_kana], b[:given_name_kana]] }
-        end
+          @none_students = @none_students.sort { |a, b| [a[:classifying], a[:grade], a[:family_name_kana], a[:given_name_kana]] <=> [b[:classifying], b[:grade], b[:family_name_kana], b[:given_name_kana]]}
+
       end
     end
   end
